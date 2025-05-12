64882e3daca3f06f63c40e518ede3964aaf744ce
Dongryul Kim <kdr0515@gmail.com>
2025-05-11 18:44:45 -0700

    Added keyboard shortcuts

--- static/olivine.js
+++ static/olivine.js
@@ -37,18 +37,15 @@ function setSidebar() {
 // Sets up correct link to journal items
 function setJournal() {
   function dateToString(d) {
-    return d.getFullYear() + '-' + String(d.getMonth()+1).padStart(2,"0") +
+    return d.getFullYear() + '-' + String(d.getMonth()+1).padStart(2,'0') +
     '-' + String(d.getDate()).padStart(2, '0');
   };
-
   const date = new Date();
-  document.getElementById('olivine-today').setAttribute('href', olivine.base_url
-    + '/journal/' + dateToString(date) + '/');
+  document.getElementById('olivine-today').setAttribute('href',
+    `${olivine.base_url}/journal/${dateToString(date)}/`);
   date.setTime(date.getTime() - 24*60*60*1000);
   document.getElementById('olivine-yesterday').setAttribute('href',
-    olivine.base_url + '/journal/' + dateToString(date) + '/');
-  document.getElementById('olivine-calendar').setAttribute('href',
-    olivine.base_url + '/journal/');
+    `${olivine.base_url}/journal/${dateToString(date)}/`);
 }
 
 // Sets up theme
@@ -87,14 +84,21 @@ function hydrateForward(id, array, url) {
       return response.text();
     })
     .then((text) => {
-      text = text.split('\n')[0];
-      var map = JSON.parse(text);
-      var li = '';
-      for (path of array) {
+      text = text.split('<script src="/livereload.js')[0];
+      const map = JSON.parse(text);
+      array = array.map((path) => {
         path = path.split('#')[0];
         if (!path.endsWith('/')) path = path + '/';
-        if (map[path]) li += '<li><a href="' + path + '">' + map[path] + '</a></li>\n';
+        return path;
+      });
+      const no_dup = [];
+      for (path of array) {
+        if (!no_dup.includes(path)) no_dup.push(path);
       }
+      const li = no_dup.map((path) => {
+        if (!map[path]) return '';
+        return `<li><a href="${path}">${map[path]}</a></li>\n`;
+      }).join('');
       document.getElementById(id).innerHTML = li;
     });
 }
@@ -104,4 +108,63 @@ function directoryExpand() {
   document.getElementById('olivine-directory').classList.remove('hidden');
   document.getElementById('olivine-directory-expand').remove();
 }
+
+// Hotkeys
+document.addEventListener("keydown", function(e) {
+  if (e.key == 'Escape') {
+    document.activeElement.blur();
+    document.getElementById("search-results").innerHTML = "";
+    document.getElementById("help-window").style.display = "none";
+    return;
+  }
+  if (e.target.nodeName != "BODY") return;
+  let scrollDist = 150;
+  switch (e.key) {
+    case 'k':
+      scrollDist *= -1;
+    case 'j':
+      if (!e.repeat) window.scrollBy({top: scrollDist, behavior: "smooth"});
+      else window.scrollBy({top: scrollDist/2, behavior: "instant"});
+      break;
+    case 'h':
+      if (olivine.lower !== undefined)
+        window.location.href = olivine.lower;
+      break;
+    case 'l':
+      if (olivine.higher !== undefined)
+        window.location.href = olivine.higher;
+      break;
+    case 'u':
+      let newUrl = window.location.href;
+      while (newUrl.endsWith('/')) newUrl = newUrl.slice(0, -1);
+      newUrl = newUrl.split('/').slice(0, -1).join('/');
+      if (!newUrl.startsWith(olivine.base_url)) break;
+      window.location.href = newUrl;
+      break;
+    case 'H':
+      window.location.href = olivine.base_url;
+      break;
+    case 'p':
+      history.back();
+      break;
+    case 'n':
+      history.forward();
+      break;
+    case '[':
+      toggleBar("leftbar");
+      break;
+    case ']':
+      toggleBar("rightbar");
+      break;
+    case '?':
+      let helpElem = document.getElementById("help-window");
+      helpElem.style.display = (helpElem.style.display === 'none') ? '' : 'none';
+      break;
+  }
+});
+document.addEventListener("keyup", function(e) {
+  if (e.target.nodeName != "BODY") return;
+  if (e.key == '/')
+    document.getElementById("search-input").focus();
+});
 
